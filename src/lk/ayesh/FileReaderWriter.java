1d5ae92545de24674de5c720d5d9828c2e5b22da
Ayeshmantha W <ayesh99747@gmail.com>
2020-02-13 20:37:34 +0530

    feat: FileReaderWriterClass added

---
+++ src/lk/ayesh/FileReaderWriter.java
@@ -0,0 +1,4 @@
+package lk.ayesh;
+
+public class FileReaderWriter {
+}
--- src/lk/ayesh/Main.java
+++ src/lk/ayesh/Main.java
@@ -3,6 +3,6 @@ package lk.ayesh;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }

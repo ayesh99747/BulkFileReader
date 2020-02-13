d1d8865cbe94e5613eb8eb2aa4e6ac0b38a031c1
Ayeshmantha W <ayesh99747@gmail.com>
2020-02-13 22:05:52 +0530

    feat: works for one file

---
+++ Input Files/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //Todo Code
+        //Single line comments
+        /*
+        * Multiline comments
+        * */
+        /**
+         * Javadoc comments
+         * */
+    }
+}
--- src/lk/ayesh/FileReaderWriter.java
+++ src/lk/ayesh/FileReaderWriter.java
@@ -1,4 +1,56 @@
 package lk.ayesh;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class FileReaderWriter {
+    private String inputFileName;
+    private String outputFileName;
+
+    public FileReaderWriter(String inputFileName, String outputFileName) {
+        this.inputFileName = inputFileName;
+        this.outputFileName = outputFileName;
+    }
+
+    public void execute() {
+        File file = new File(this.inputFileName);
+        List<String> inputLines = new ArrayList<>();
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            System.out.println("Lines appended to file");
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                System.out.println(line);
+                inputLines.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error! Unable to find file!");
+        }
+        this.writeToFile(inputLines);
+    }
+
+    private void writeToFile(List<String> lines) {
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(this.outputFileName, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+
+            bufferedWriter.write(this.inputFileName);
+            bufferedWriter.newLine();
+            bufferedWriter.newLine();
+
+            for (String line : lines) {
+                bufferedWriter.write(line);
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.newLine();
+
+            bufferedWriter.close();
+        } catch (IOException e) {
+            System.out.println("Unable to find file!");
+        }
+    }
 }
